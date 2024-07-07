c2766bcad9bd58ed34842ce2333dd3da839685c7
insidedw <insidedw@gmail.com>
2024-07-07 22:54:19 +0900

    integrate renderToPipeableStream

--- src/client.js
+++ src/client.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrateRoot } from 'react-dom/client'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
+import { HydrationBoundary, QueryClientProvider } from '@tanstack/react-query'
+import { getQueryClient } from './routes'
+const queryClient = getQueryClient()
 
-ReactDOM.hydrate(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+const dehydratedState = window.__REACT_QUERY_STATE__ ?? {}
+
+hydrateRoot(
   document.getElementById('root'),
+  <QueryClientProvider client={queryClient}>
+    <HydrationBoundary state={dehydratedState}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HydrationBoundary>
+  </QueryClientProvider>,
 )
--- src/server.js
+++ src/server.js
@@ -2,9 +2,13 @@ import path from 'path'
 import fs from 'fs'
 import React from 'react'
 import express from 'express'
-import { renderToString } from 'react-dom/server'
+import { renderToPipeableStream } from 'react-dom/server'
 import App from './components/App'
 import { StaticRouter } from 'react-router-dom/server'
+import { getQueryClient } from './routes'
+import { dehydrate, QueryClientProvider } from '@tanstack/react-query'
+import { customFetch } from './components/queries'
+import { PassThrough, Readable } from 'stream'
 
 const app = express()
 app.use(express.static('dist'))
@@ -18,8 +22,9 @@ const html = `
     <title>React SSR</title>
 </head>
 <body>
-<div id="root"></div>
+<div id="root">{{STREAM}}</div>
 </body>
+<script>window.__REACT_QUERY_STATE__={{INITIAL_STATE}};</script>
 </html>
 `
 
@@ -34,19 +39,84 @@ const styles = Object.keys(manifest)
   .map((key) => `<link rel="stylesheet" type="text/css" href="${manifest[key]}">`)
   .join('\n')
 
-app.get('*', (req, res) => {
-  const appString = renderToString(
-    <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>,
+const [head, footer] = html.split('{{STREAM}}')
+const refinedHead = head.replace('</head>', `${styles}</head>`)
+const refinedFooter = footer.replace('</body>', `${scripts}</body>`)
+
+function elementToReadable(element) {
+  /**
+   * pipe 함수가 WritableStream으로 데이터를 전달해주는 함수
+   * react component만 스트림으로 한다면 바로 pipe(res) 하면 되는데,
+   * 그 외 html markup도 stream으로 보내야 하므로, transfrom stream을 이용해서 데이터 그대로 읽기 스트림으로 전달한다
+   * @type {module:stream.internal.PassThrough}
+   */
+  const duplex = new PassThrough()
+
+  return new Promise((resolve, reject) => {
+    /**
+     *  HTML을 제공된 쓰기 가능한 Node.js 스트림으로 출력
+     */
+    const { pipe, abort } = renderToPipeableStream(element, {
+      onShellReady() {
+        console.log('[Streaming SSR] onShellReady')
+        resolve(pipe(duplex))
+      },
+      onShellError(error) {
+        abort()
+        reject(error)
+      },
+      onError: (error) => {
+        duplex.emit('onError', error)
+      },
+    })
+  })
+}
+async function* streamHTML(head, body, footer) {
+  yield head
+  console.log('[Streaming SSR] head rendered')
+  let i = 0
+  for await (const chunk of body) {
+    yield chunk
+    i++
+    console.log(`[Streaming SSR] chunk ${i} rendered`)
+  }
+
+  yield footer
+  console.log('[Streaming SSR] footer rendered')
+}
+
+app.get('*', async (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.status(200)
+
+  const queryClient = getQueryClient()
+
+  queryClient.prefetchQuery({ queryKey: ['image', '1'], queryFn: () => customFetch() })
+  const dehydratedState = dehydrate(queryClient)
+  const footerWithQueryState = refinedFooter.replace('{{INITIAL_STATE}}', `${JSON.stringify(dehydratedState)}`)
+
+  const vnode = (
+    <QueryClientProvider client={queryClient}>
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    </QueryClientProvider>
   )
 
-  return res.send(
-    html
-      .replace('<div id="root"></div>', `<div id="root">${appString}</div>`)
-      .replace('</head>', `${styles}</head>`)
-      .replace('</body>', `${scripts}</body>`),
-  )
+  try {
+    const ssrStream = Readable.from(streamHTML(refinedHead, await elementToReadable(vnode), footerWithQueryState))
+    ssrStream.on('error', (error) => {
+      console.log(`ssrStream!`, error.message)
+    })
+    ssrStream.on('close', () => {
+      queryClient.clear()
+      console.log(`queryClient clear!`)
+    })
+
+    ssrStream.pipe(res)
+  } catch (e) {
+    console.log('error', e)
+  }
 })
 
 app.listen(3000, () => {
